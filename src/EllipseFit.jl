cb7d54b47691befa6b97d487545f39247bfd98e6
Casey Haaland <caseychandlerh@yahoo.com>
2018-08-07 01:12:17 -0700

    Export solver methods from package

--- src/EllipseFit.jl
+++ src/EllipseFit.jl
@@ -1,5 +1,6 @@
 module EllipsePlot
-    export quadform2ellipse_coords, parametric2ellipse_coords 
+    export quadform2ellipse_coords, parametric2ellipse_coords, rotate_mat2d, 
+           vandermonde, gauss_newton, levenberg_marquardt
 
     ####################################
     # Utils
@@ -8,5 +9,14 @@ module EllipsePlot
     include("utils/rotate_mat2d.jl");
     include("utils/parametric2ellipse_coords.jl");            
     include("utils/quadform2ellipse_coords.jl");
+    include("utils/vandermonde.jl");
+
+    ####################################
+    # Solvers
+    #################################### 
+    include("solvers/gauss_newton.jl");
+    include("solvers/levenberg_marquardt.jl");
+    include("solvers/newton_raphson.jl");
+    include("solvers/grad_desc.jl");
 
 end
